8e367297a2d312c723f2c68290a6bdf2dd621558
Abhijeet Sharma <as3575823@gmail.com>
2026-07-02 18:43:03 +0530

    Configure API URL for production

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const path = require('path');
+
 
 dotenv.config();
 connectDB();
@@ -25,17 +25,9 @@ app.use('/api/analytics', require('./routes/analyticsRoutes'));
 app.use("/api/ai", require("./routes/aiRoutes"));
 
 // Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('EasyMart API is running in Development mode...');
-  });
-}
+app.get("/", (req, res) => {
+  res.send("EasyMart API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
